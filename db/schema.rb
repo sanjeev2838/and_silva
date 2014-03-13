2bb31dd34d164390405695d8b76ed768fdc47002
Christopher de Alba <superototus@gmail.com>
2014-03-13 04:05:03 -0700

    Change several buttons to bootstrap theme, also enable glyphs by finding and replacing instances of 'fonts' with 'assets' in application.min.css

--- db/schema.rb
+++ db/schema.rb
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 20140307143842) do
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["profile_name"], name: "index_users_on_profile_name"
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
